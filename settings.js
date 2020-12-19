5d55bb47baef5a56333b458e289d0899f8890a49
Taylor Bellegue <tbellegue@gmail.com>
2020-12-19 00:14:58 -0500

    getting rid of dups

--- settings.js
+++ settings.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 module.exports = {
     secrets: require("./config/secrets.json"),
     calendarConfig: require("./config/calendarsettings.js")
 };
-=======
-module.exports = {
-    secrets: require("./config/secrets.json"),
-    calendarConfig: require("./config/calendarsettings.js")
-};
->>>>>>> 0dfea8a993486345caba2d6c5eddfc5b3eaf9020
